6e60890a2a1635a5091fba443003b0e9e2560f1c
erda1010 <57817522+erda1010@users.noreply.github.com>
2019-11-18 01:24:07 +0100

    Update ImageSearch.js

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
 export default class ImageSearch extends Component {
+  
     render() {
-        const{value,handleSubmit,handleChange}=this.props
+        const { value, handleSubmit, handleChange, handleSort, handleSortTop, handleSortTime, handleSortRising } = this.props
         return (
             <React.Fragment>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 text-center">
                             <h1>Search one of categories:
-                                <span className="strong-text">hot</span>
+                                <span  className="strong-text">hot</span>
                                 <span className="strong-text">user</span>
                                 <span className="strong-text">top</span>
                             </h1>
@@ -17,17 +18,28 @@ export default class ImageSearch extends Component {
                                 <label htmlFor="search">
                                 </label>
                                 <div className="input-group">
-                                    <input type="text" name="search" 
-                                    placeholder="search for photos" 
-                                    className="form-control" 
-                                    value={value}
-                                    onChange={handleChange}
+                                    <input type="text" name="search"
+                                        placeholder="search for photos"
+                                        className="form-control"
+                                        value={value}
+                                        onChange={handleChange}
                                     />
-                                    <button type="submit"className="button-search bg-primary">
-                                    <i class="fa fa-search" aria-hidden="true"></i>Search
+                                    <button type="submit" className="button-search bg-primary">
+                                        <i class="fa fa-search" aria-hidden="true"></i>Search
                                     </button>
                                 </div>
                             </form>
+                            <div className="option-grop">
+                                <span>Sort: </span>
+                                <button type="button" className="options"
+                                onClick={handleSort}>viral</button>
+                                <button type="button" className="options"
+                                 onClick={handleSortTop}>top</button>
+                                <button type="button" className="options"
+                                 onClick={handleSortTime}>time</button>
+                                <button type="button" className="options"
+                                 onClick={handleSortRising}>rising</button>
+                            </div>
                         </div>
                     </div>
                 </div>
